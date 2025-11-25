d78464ffe7ee7cb644d1feb9e8c1a6e37289fbcf
hassane31 <hassane31@kali.SAMSUNG>
2025-11-25 23:11:47 +0100

    making my first tool in linux(kali)

---
+++ H_checker/mytool.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "===== H_cheker Kali Tool ====="
+echo -n "Enter target (IP or domain): "
+read target
+
+echo "[*] Pinging $target ..."
+ping -c 4 $target > result.txt
+
+echo "[+] Done!"
+echo "[+] Results saved in result.txt"
+echo "================================"
