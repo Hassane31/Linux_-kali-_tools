19530ecd7103801ab4ed8489168bc85973178f0f
hassane31 <hassane31@kali.SAMSUNG>
2025-11-25 23:29:58 +0100

    add Iwconfig to resultstxt

--- H_checker/mytool.sh
+++ H_checker/mytool.sh
@@ -5,8 +5,12 @@ echo -n "Enter target (IP or domain): "
 read target
 
 echo "[*] Pinging $target ..."
-ping -c 4 $target > result.txt
+iwconfig wlan0 | ping -c 4 $target > result.txt
 
+echo "">>result.txt
+echo "============Iwconfig==============">>result.txt
+iwconfig>>result.txt
+ 
 echo "[+] Done!"
 echo "[+] Results saved in result.txt"
 echo "================================"
